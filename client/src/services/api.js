ac30c4e8f0cd6cd271a51427287d2bd610619a74
thetelgote <sushanttelgote4@gmail.com>
2026-06-10 18:26:55 +0530

    Updated production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://sushant-assignment.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
